548a1dd344a8b678a563208276288c13b17568ce
ibraloyza <ibraahim3523@gmail.com>
2024-07-26 12:58:18 +0300

    added html contant

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My_project</title>
+</head>
+<body>
+   <h1 class="hero">
+    this user's name is ⚒️⛏️🔧
+   </h1>
+   <div class="container">
+    <div class="holder">
+        <form action="index.php" method="POST">
+            <div class="wrapper">
+                <input type="text" placeholder="Entar UserNAme ?" name="name" >
+            </div>
+            <br>
+            <br>
+            <div class="wrapper">
+                <input type="submit" value="submit" name="submit">
+            </div>
+        </form>
+    </div>
+   </div>
+   <h4 class="footer"> Made with <span❤️</span>by ibra loyza</h4>
+
+</body>
+</html>
